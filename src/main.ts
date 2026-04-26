dec3de428314386c84fa1c1507b7b55e87d28270
Masamune903 <masamune_903@outlook.jp>
2026-04-26 23:48:21 +0900

    importパスを @ にする対象に main.ts が漏れていたため実行

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { Hono } from "hono";
-import { createHandlers } from "./feature/Task/handler/web-api/handler.ts";
-import { createDependencies } from "./deps/CompositionRoot.ts";
+import { createHandlers } from "@feature/Task/handler/web-api/handler.ts";
+import { createDependencies } from "@deps/CompositionRoot.ts";
 
 function main() {
   const app = new Hono();
@@ -8,7 +8,8 @@ function main() {
   const deps = createDependencies("in-memory");
   createHandlers(app, deps);
   
-  Deno.serve({ port: 8080 }, app.fetch);
+  const port = Number(Deno.env.get("DENO_PORT") ?? 80);
+  Deno.serve({ port }, app.fetch);
 }
 
 main();
