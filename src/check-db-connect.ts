52ef4debbb4fc9eb2589e2c2aee65b35a708ead6
Masamune903 <masamune_903@outlook.jp>
2026-04-27 20:51:23 +0900

    必要な環境変数が無いときに把握しやすいようバリデーションを追加

--- src/check-db-connect.ts
+++ src/check-db-connect.ts
@@ -1,26 +1,43 @@
+// docker compose exec app sh -c "deno run -A src/check-db-connect.ts"
 // http://github.com/denodrivers/postgres
 
 // コンテナ同士の接続で一時的に利用するため
 // deno-lint-ignore no-import-prefix no-unversioned-import
 import { Client, ClientOptions } from "jsr:@db/postgres";
 
-const clientOptions: ClientOptions = {
-  hostname: Deno.env.get("DB_HOST"),
-  port: Deno.env.get("DB_PORT"),
-  database: Deno.env.get("DB_NAME"),
-  user: Deno.env.get("DB_APP_USER"),
-  password: Deno.env.get("DB_APP_PASSWORD"),
-};
+async function main() {
+  const clientOptions: ClientOptions = {
+    hostname: getRequiredEnv("DB_HOST"),
+    port: getRequiredNumEnv("DB_PORT"),
+    database: getRequiredEnv("DB_NAME"),
+    user: getRequiredEnv("DB_APP_USER"),
+    password: getRequiredEnv("DB_APP_PASSWORD"),
+  };
+  
+  const client = new Client(clientOptions);
 
-console.log(clientOptions);
+  await client.connect();
 
-const client = new Client(clientOptions);
+  {
+    const result = await client.queryObject("SELECT * FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema'");
+    console.log(result.rows);
+  }
 
-await client.connect();
-
-{
-  const result = await client.queryObject("SELECT * FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema'");
-  console.log(result.rows);
+  await client.end();
 }
 
-await client.end();
+function getRequiredEnv(name: string): string {
+  const value = Deno.env.get(name);
+  if (value === undefined || value.trim() === "")
+    throw new Error(`必須環境変数 ${name} が設定されていません。`);
+  return value;
+}
+
+function getRequiredNumEnv(name: string): string {
+  const value = getRequiredEnv(name);
+  if (Number.isNaN(+value))
+    throw new Error(`環境変数 ${name} の値が数値ではありません: ${value}`);
+  return value;
+}
+
+main();
