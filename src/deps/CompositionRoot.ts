2da2b73eab40b375c6540dd4c5e5ab437672d6fb
Masamune903 <masamune_903@outlook.jp>
2026-04-28 07:25:03 +0900

    createDependencies に DB初期化設定を追加

--- src/deps/CompositionRoot.ts
+++ src/deps/CompositionRoot.ts
@@ -8,8 +8,10 @@ import { ITaskRepository } from "@feature/Task/domain/TaskRepository.ts";
 import { InMemoryTaskRepository } from "@feature/Task/repository/InMemoryTaskRepository.ts";
 import { GetAllTasksUseCase } from "@feature/Task/usecase/GetAllTasksUseCase.ts";
 import { SearchTasksByStatusUseCase } from "@feature/Task/usecase/SearchTasksByStatusUseCase.ts";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { migrate } from "drizzle-orm/node-postgres/migrator";
 
-export type Environment = "in-memory";
+export type Environment = "in-memory" | "pg-drizzle";
 
 export type Dependencies = {
   readonly taskRepository: ITaskRepository;
@@ -26,7 +28,7 @@ type DependencyOptions = {
   readonly clock?: Clock;
 };
 
-export function createDependencies(environment: Environment, options: DependencyOptions = {}): Dependencies {
+export function createDependencies(environment: Environment, options: DependencyOptions = {}): Dependencies | Promise<Dependencies> {
   const {
     idGenerator = new UUIDv4Generator(),
     clock = new SystemClock()
@@ -35,6 +37,8 @@ export function createDependencies(environment: Environment, options: Dependency
   switch (environment) {
     case "in-memory":
       return createInMemoryDependencies(idGenerator, clock);
+    // case "pg-drizzle":
+    //   return createPgDrizzleDependencies(idGenerator, clock);
     default:
       throw new Error(`Unknown environment: ${environment}`);
   }
@@ -49,7 +53,37 @@ function createInMemoryDependencies(idGenerator: IdGenerator, clock: Clock): Dep
   const searchTasksByStatusUseCase = new SearchTasksByStatusUseCase(taskRepository);
   const updateTaskUseCase = new UpdateTaskUseCase(taskRepository, clock);
   const deleteTaskUseCase = new DeleteTaskUseCase(taskRepository);
-  
+
+  return { 
+    taskRepository,
+    createTaskUseCase,
+    getAllTasksUseCase,
+    findTaskByIdUseCase,
+    searchTasksByStatusUseCase,
+    updateTaskUseCase,
+    deleteTaskUseCase,
+  };
+}
+
+async function createPgDrizzleDependencies(idGenerator: IdGenerator, clock: Clock) {
+  // # データベースの用意
+  const db_url = Deno.env.get("DB_APP_URL");
+  if (db_url == null)
+    throw new Error("環境変数 DB_APP_URL が指定されていません");
+
+  const db = drizzle(db_url);
+  await migrate(db, { migrationsFolder: "./drizzle" });
+
+  // # リポジトリ作成
+  const taskRepository = {} as ITaskRepository; // new PgDrizzleTaskRepository();
+
+  // # ユースケース作成
+  const createTaskUseCase = new CreateTaskUseCase(taskRepository, idGenerator, clock);
+  const findTaskByIdUseCase = new FindTaskByIdUseCase(taskRepository);
+  const getAllTasksUseCase = new GetAllTasksUseCase(taskRepository);
+  const searchTasksByStatusUseCase = new SearchTasksByStatusUseCase(taskRepository);
+  const updateTaskUseCase = new UpdateTaskUseCase(taskRepository, clock);
+  const deleteTaskUseCase = new DeleteTaskUseCase(taskRepository);
 
   return { 
     taskRepository,
